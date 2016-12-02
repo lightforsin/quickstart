5871f1fa19affa795e77c1031a70615da8b80dd2
lightforsin <paul.bordea@ymail.com>
2016-12-02 23:34:24 +0200

    Add http, observables, in-memory API

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -10,13 +10,13 @@ import { HeroService } from './hero.service';
   	styleUrls: [ 'dashboard.component.css' ]
 })
 
-export class DashboardComponent implements OnInit { 
+export class DashboardComponent implements OnInit {
 	heroes: Hero[] = [];
 
 	constructor(private heroService: HeroService) {}
 
 	ngOnInit(): void {
 		this.heroService.getHeroes()
-		.then(heroes => this.heroes = heroes.slice(1, 6));
+		.then(heroes => this.heroes = heroes.slice(1, 5));
 	}
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   moduleId: module.id,
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
-  styleUrls: ['heroes.component.css'] 
+  styleUrls: ['heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
@@ -35,5 +35,26 @@ export class HeroesComponent implements OnInit {
 
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+
+    if(!name) { return; }
+
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
   }
 }
